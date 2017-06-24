ab9ad14a09ffb1ece1081c1f8fd329bfac323f10
Dan O'Connor <dpo3ys@virginia.edu>
2017-06-24 12:19:27 -0700

    Initial commit

---
+++ SitStandTimer/TimeManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml;
+
+namespace SitStandTimer
+{
+    public enum Mode
+    {
+        Sit,
+        Stand
+    }
+
+    public class TimeManager
+    {
+        private static TimeManager _instance;
+        public static TimeManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new TimeManager();
+                }
+
+                return _instance;
+            }
+        }
+
+        private TimeManager()
+        {
+            // Default to standing
+            CurrentMode = Mode.Stand;
+            _currentModeStart = DateTime.Now;
+
+            _timer = new DispatcherTimer()
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+            _timer.Tick += timerTick;
+        }
+
+        private void timerTick(object sender, object e)
+        {
+            
+        }
+
+        public Mode CurrentMode { get; private set; }
+
+        private DateTime _currentModeStart;
+        private DispatcherTimer _timer;
+
+        private static readonly Dictionary<Mode, TimeSpan> _modeIntervals = new Dictionary<Mode, TimeSpan>()
+        {
+            { Mode.Sit, TimeSpan.FromMinutes(60) },
+            { Mode.Stand, TimeSpan.FromMinutes(60) }
+        };
+    }
+}
---
+++ SitStandTimer/ViewModels/MainPageVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SitStandTimer.ViewModels
+{
+    public class MainPageVM
+    {
+        public MainPageVM()
+        {
+            ModeText = "STAND";
+            TimeLeftText = "59 minutes until you can sit.";
+        }
+
+        public string ModeText { get; private set; }
+        public string TimeLeftText { get; private set; }
+    }
+}
